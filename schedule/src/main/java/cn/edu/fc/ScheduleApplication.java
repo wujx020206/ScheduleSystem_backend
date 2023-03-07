659dfeb074f1adf64f6f19dbb6329c3963103552
吴佳熙 <1006961384@qq.com>
2023-03-07 15:32:20 +0800

    schedule模块

---
+++ schedule/src/main/java/cn/edu/fc/ScheduleApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.fc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = {"cn.edu.fc.javaee.core",
+        "cn.edu.fc.schedulesystem.schedule"})
+@MapperScan("cn.edu.xmu.schedulesystem.schedule.mapper")
+@EnableFeignClients
+@EnableDiscoveryClient
+public class ScheduleApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

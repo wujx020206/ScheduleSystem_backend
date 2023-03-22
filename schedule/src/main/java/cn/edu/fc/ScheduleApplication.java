250f6c1f5b8358ad8f4b36d9ba4b2738485ac3b9
吴佳熙 <1006961384@qq.com>
2023-03-23 00:35:53 +0800

    修改schedule模块application

--- schedule/src/main/java/cn/edu/fc/ScheduleApplication.java
+++ schedule/src/main/java/cn/edu/fc/ScheduleApplication.java
@@ -1,6 +1,7 @@
 package cn.edu.fc;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,6 +13,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 public class ScheduleApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ScheduleApplication.class, args);
     }
 }

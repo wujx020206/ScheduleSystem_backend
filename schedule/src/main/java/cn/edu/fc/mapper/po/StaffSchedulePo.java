bedda58eb5af917b9a026688e84b028a4389aa6c
吴佳熙 <1006961384@qq.com>
2023-03-29 08:32:22 +0800

    配置schedule模块

--- schedule/src/main/java/cn/edu/fc/mapper/po/StaffSchedulePo.java
+++ schedule/src/main/java/cn/edu/fc/mapper/po/StaffSchedulePo.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "schedule_staff_schedule")
+@Table(name = "schedule")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
